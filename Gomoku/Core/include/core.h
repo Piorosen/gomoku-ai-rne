15a1a86cb177d66ac2a0746044553f9f74203fc1
Piorosen <aoikazto@gmail.com>
2021-08-25 12:49:59 +0900

    힝구힝구

--- Gomoku/Core/include/core.h
+++ Gomoku/Core/include/core.h
@@ -1,2 +1,4 @@
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_sinks.h"
 
 int haha(int a, int b);
--- Gomoku/main.cpp
+++ Gomoku/main.cpp
@@ -1,5 +1,8 @@
+#include <core.h>
+#include <graphic.h>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char **argv)
 {
-    // haha(20, 30);
+    haha(20, 30);
 }
