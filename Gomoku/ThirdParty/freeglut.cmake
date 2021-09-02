bc6f448ef9dfec8590a36cd13a00a8e05010759a
Piorosen <aoikazto@gmail.com>
2021-09-02 15:33:50 +0900

    윈도우도 지원하는가?

--- Gomoku/ThirdParty/freeglut.cmake
+++ Gomoku/ThirdParty/freeglut.cmake
@@ -1,17 +1,2 @@
-include(ExternalProject)
-
-# Download RapidJSON
-ExternalProject_Add(
-    freeglut
-    GIT_REPOSITORY "https://github.com/dcnieho/FreeGLUT.git"
-    GIT_TAG 349a23dcc1264a76deb79962d1c90462ad0c6f50
-    TIMEOUT 10
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    UPDATE_COMMAND ""
-)
-
-# Prepare RapidJSON (RapidJSON is a header-only library)
-ExternalProject_Get_Property(freeglut source_dir)
-set(FREEGLUT_INCLUDE_DIR ${source_dir}/freeglut/freeglut/include)
+set(FREEGLUT_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/ThirdParty/freeglut/include)
+set(FREEGLUT_LIBRARY_DIR ${CMAKE_SOURCE_DIR}/ThirdParty/freeglut/lib)
