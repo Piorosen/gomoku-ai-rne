d2b1845094ff13a666bed00c5193836abcbabe81
Piorosen <aoikazto@gmail.com>
2021-09-13 00:28:19 +0900

    ㅁㅈㄷㄹ
    ㅁㅈㄷ

--- Gomoku/ThirdParty/stb.cmake
+++ Gomoku/ThirdParty/stb.cmake
@@ -1,17 +1,5 @@
-include(ExternalProject)
 
-# Download RapidJSON
-ExternalProject_Add(
-    stb
-    GIT_REPOSITORY "https://github.com/nothings/stb"
-    GIT_TAG c0c982601f40183e74d84a61237e968dca08380e
-    TIMEOUT 10
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    UPDATE_COMMAND ""
-)
+    # GIT_REPOSITORY "https://github.com/nothings/stb"
 
-ExternalProject_Get_Property(stb source_dir)
-set(STB_INCLUDE_DIR ${source_dir})
+set(STB_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/ThirdParty/stb)
 
