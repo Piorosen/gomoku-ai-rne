d98988b15f796e59ef7b07eaa16578641585f31c
Piorosen <aoikazto@gmail.com>
2021-09-26 03:50:09 +0900

    Update windows.bat

--- Gomoku/windows.bat
+++ Gomoku/windows.bat
@@ -6,7 +6,9 @@ cmd.exe /C .\ThirdParty\cmake\cmake-3.21.3-windows-x86_64.msi
 cmd.exe /C .\ThirdParty\vcpkg\bootstrap-vcpkg.bat
 cmd.exe /C .\ThirdParty\vcpkg\vcpkg install freeglut glew
 cmd.exe /C .\ThirdParty\vcpkg\vcpkg integrate install
-cmd.exe /C cmake -G "Visual Studio 16 2019" -A Win32
+
+:: cmd.exe /C cmake -G "Visual Studio 16 2019" -A Win32
+cmd.exe /C cmake -A Win32
 
 echo ##### End Visual Studio Project #####
 
