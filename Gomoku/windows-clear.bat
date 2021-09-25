d8e80b6c08dcc5bcf472f76a418fd6e4910f7a7d
Piorosen <aoikazto@gmail.com>
2021-09-26 00:01:19 +0900

    Update windows-clear.bat

--- Gomoku/windows-clear.bat
+++ Gomoku/windows-clear.bat
@@ -1,7 +1,10 @@
 @echo off
-echo ##### Remove Visual Studio Projecct #####
-cmd.exe /C .\ThirdParty\vcpkg\vcpkg integrate remove
-echo ##### Remove Visual Studio Projecct #####
+echo ##### Make Visual Studio Projecct #####
 
+cmd.exe /C .\ThirdParty\vcpkg\bootstrap-vcpkg.bat
+cmd.exe /C .\ThirdParty\vcpkg\vcpkg install freeglut glew
+cmd.exe /C .\ThirdParty\vcpkg\vcpkg integrate install
+cmd.exe /C cmake -G "Visual Studio 16 2019" -A Win32
 
+echo ##### End Visual Studio Project #####
 pause
