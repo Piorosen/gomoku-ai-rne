d20d652fa676381ebd3d075b1ee52576d595ea94
Piorosen <aoikazto@gmail.com>
2021-09-25 23:54:44 +0900

    배치파일 완성!

--- Gomoku/windows-clear.bat
+++ Gomoku/windows-clear.bat
@@ -1,3 +1,7 @@
 @echo off
 echo ##### Remove Visual Studio Projecct #####
-cmd.exe /C ./ThirdParty/vcpkg/vcpkg integrate remove
+cmd.exe /C .\ThirdParty\vcpkg\vcpkg integrate remove
+echo ##### Remove Visual Studio Projecct #####
+
+
+pause
