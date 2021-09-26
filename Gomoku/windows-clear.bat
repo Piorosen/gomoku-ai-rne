47ece818479f549a561cc97400cd5f7ec59d4c15
Piorosen <aoikazto@gmail.com>
2021-09-26 19:06:22 +0900

    a

--- Gomoku/windows-clear.bat
+++ Gomoku/windows-clear.bat
@@ -1,7 +1,6 @@
 @echo off
 echo ##### Remove : Visual Studio Projecct #####
 
-cmd.exe /C .\ThirdParty\cmake\cmake-3.21.3-windows-x86_64.msi
 cmd.exe /C .\ThirdParty\vcpkg\vcpkg integrate remove
 
 echo ##### Remove : Visual Studio Project #####
