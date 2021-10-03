aa3f90daada29cc657b27301a0135ef77a372c8a
Piorosen <aoikazto@gmail.com>
2021-10-03 10:44:44 +0900

    Update main.cpp

--- Gomoku/main.cpp
+++ Gomoku/main.cpp
@@ -29,6 +29,16 @@ int main(int argc, char **argv)
     manager::shared->setNotFoundAi(notFoundAi);
     manager::shared->setWinCheck(winCheck);
     manager::shared->setDefaultAiCalculateTimeOut(500);
+    // manager::shared->showPredictNumer(false);
+    // manager::shaerd->showAllNumber(true);
+
+    /**
+ * TODO : 
+ * 1. 인공지능 모델 재 학습 기능 구현하기
+ * 2. 숫자 지우기 모드
+ * 3. 
+ * 
+ */
 
     manager::shared->run();
 }
