88ae00585b3b953a0155ae501d746833e940c6b1
Piorosen <aoikazto@gmail.com>
2021-10-03 10:54:12 +0900

    vcpkg 를 프로젝트에서 다운로드 받지 않고, 윈도우용 인스톨러에서 설치하도록 함.

--- Gomoku/windows.bat
+++ Gomoku/windows.bat
@@ -1,10 +1,6 @@
 @echo off
 echo ##### Make Visual Studio Projecct #####
 
-cmd.exe /C .\ThirdParty\vcpkg\bootstrap-vcpkg.bat
-cmd.exe /C .\ThirdParty\vcpkg\vcpkg install freeglut glew
-cmd.exe /C .\ThirdParty\vcpkg\vcpkg integrate install
-
 :: cmd.exe /C cmake -G "Visual Studio 16 2019" -A Win32
 cmd.exe /C cmake -A Win32
 
